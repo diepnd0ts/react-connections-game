e81691416537ddc84cbbd10981a9a61c12b8e84b
Bryan Diep <diepndots@gmail.com>
2024-02-14 15:38:03 -0800

    changes

--- src/lib/data.js
+++ src/lib/data.js
@@ -4,22 +4,27 @@ export const CONNECTION_GAMES = [
       category: "Core Memories with You",
       words: ["Shucking Oysters", "Palisades", "Catching the Train", "Bar Hopping"],
       difficulty: 4,
+      imageSrc: "travel.jpg",
     },
     {
       category: "Places We've Been To",
       words: ["Italy", "Miami", "Tulum", "Texas"],
       difficulty: 1,
+      imageSrc: "https://github.com/diepnd0ts/react-connections-game/blob/main/src/images/travel.jpg",
     },
     {
       category: "Things I Love About You",
       words: ["Eyes", "Heart", "Smile", "Hair"],
       difficulty: 2,
+      imageSrc: "https://i.ibb.co/4MwD7zv/Connections-3.png",
     },
 
     {
       category: "Things I Love Doing with You",
       words: ["Climb", "Board Games", "Travel", "Errands"],
       difficulty: 3,
+      imageSrc:
+        "https://i.ibb.co/V2vnr5P/boys2men-2-d53afdddf480401fac1204a3954aa5f6.webp",
     },
   ],
 ];
